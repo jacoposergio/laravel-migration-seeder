2e55ba5ac0a286bfe0e7487d7dfbc285141e4745
jacoposergio <djago32@gmail.com>
2022-08-29 16:16:43 +0200

    done

--- database/seeds/PackagesTableSeeder.php
+++ database/seeds/PackagesTableSeeder.php
@@ -12,16 +12,21 @@ class PackagesTableSeeder extends Seeder
      * @return void
      */
     public function run(Faker $faker)
-    {
+    {  
+         $vehicle_types = [
+        'Bus',
+        'Aereoplane',
+        'Car'
+         ];
         // creare un x numero di righe nella tabella packages
-        for ($i = 0; $i <5 ; $i++){
+        for ($i = 1; $i <=5; $i++) {
             $new_package = new Package();
             $new_package->country = $faker->country();
-            $new_package->vehicle =  $faker->randomElement($vehicle);
-            $new_package->distance = $faker->randomNumber(5, true);
+            $new_package->vehicle =  $faker->randomElement($vehicle_types);
+            $new_package->distance = $faker->randomNumber(2, true);
             $new_package->passengers = $faker->randomNumber(2, true);
             $new_package->is_promo = $faker->boolean();
-            $new_package->description = $faker->sentences();
+            $new_package->description = $faker->paragraphs(7, true);
             $new_package->price = $faker->randomNumber(4, false);
             //salvare la riga
             $new_package->save();
