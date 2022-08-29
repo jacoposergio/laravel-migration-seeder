ec157396b2aa09157f6df2d72a430e1bd6361e4a
jacoposergio <djago32@gmail.com>
2022-08-29 15:53:34 +0200

    fakerdone

---
+++ database/seeds/PackagesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use\App\Package;
+use Faker\Generator as Faker;
+
+class PackagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        // creare un x numero di righe nella tabella packages
+        for ($i = 0; $i <5 ; $i++){
+            $new_package = new Package();
+            $new_package->country = $faker->country();
+            $new_package->vehicle =  $faker->randomElement($vehicle);
+            $new_package->distance = $faker->randomNumber(5, true);
+            $new_package->passengers = $faker->randomNumber(2, true);
+            $new_package->is_promo = $faker->boolean();
+            $new_package->description = $faker->sentences();
+            $new_package->price = $faker->randomNumber(4, false);
+            //salvare la riga
+            $new_package->save();
+        }
+        
+    }
+}
